df742859a952c1d67dcfbe520aecf16709c1312f
Wade <differ505@gmail.com>
2026-06-01 15:59:09 +0800

    Configure qingxidesign.tw as site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+    site: 'https://qingxidesign.tw',
     output: 'server',  // 啟用服務器模式以支持中間件
     adapter: vercel(),
     vite: {
